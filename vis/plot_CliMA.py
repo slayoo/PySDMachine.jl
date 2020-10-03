98ea2c4b4891825db1e70cebd87eecc654bd48ca
Anna Jaruga <ajaruga@caltech.edu>
2020-10-02 17:32:14 -0700

    add dry density plot

--- vis/plot_CliMA.py
+++ vis/plot_CliMA.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
@@ -5,20 +6,38 @@ import matplotlib as mpl
 from netCDF4 import Dataset
 from copy import copy
 
-# TODO:
-# - don't hardcode file names
-# - plot dry density, not the total
+# TODO - should be read in from CLIMAParameters.jl
+rho_l = 1e3
+Mv = 18e-3
+Md = 28.97e-3
+Rd = 8.3144598  / Md
+eps = Mv/Md
 
+def dry_density(rho, p, T, q_tot, q_vap, q_liq):
+    """
+    Computes dry air density based on air density and water specific humidites.
+    """
+    q_dry = 1. - q_tot
+    q_vap[q_vap < 0.0] = 0.0
+    q_liq[q_liq < 0.0] = 0.0
+
+    # TODO - what should we do with the liquid water?
+    rho_d = (rho - p / Rd / T / (1/q_vap + 1/eps - 1.)) / q_dry
+
+    return rho_d
+
+# read the data
+# assumes there is only one aux and one state output file
 clima_output = "../ClimateMachine.jl/output/"
-
-# TODO - find those file automatically
-aux_f = clima_output + "KinematicModel_DumpAux-2020-10-01T15.11.44.564.nc"
-state_f = clima_output + "KinematicModel_DumpState-2020-10-01T15.11.44.564.nc"
+for fname in os.listdir(clima_output):
+    if fname.startswith("KinematicModel_DumpAux"):
+        aux_f = clima_output + fname
+    if fname.startswith("KinematicModel_DumpState"):
+        state_f = clima_output + fname
 
 aux_data = Dataset(aux_f, 'r')
 state_data = Dataset(state_f, 'r')
 
-# read the data
 y_idx = 5
 z = state_data["z"][:]
 x = state_data["x"][:]
@@ -58,16 +77,20 @@ fig.colorbar(im2, ax=axs[1])
 plt.tight_layout()
 plt.show()
 
-def dry_density(rho, q_tot, q_vap, q_liq):
-    #TODO
-    return rho
-
 #plot initial profiles of T, p, RH, rho (TODO - should be dry density)
+rho_d = dry_density(rho[: , :, :],\
+                    data_dict["p"],\
+                    data_dict["T"],\
+                    data_dict["q_tot"],\
+                    data_dict["q_vap"],\
+                    data_dict["q_liq"])
 fig, axs = plt.subplots(2,2, figsize=(8, 8))
 im1 = axs[0, 0].plot(np.average(data_dict["T"][t_idx, :, :], axis=1), z)
 im2 = axs[0, 1].plot(np.average(data_dict["p"][t_idx, :, :], axis=1), z)
 im3 = axs[1, 0].plot(np.average(data_dict["RH"][t_idx, :, :], axis=1), z)
-im4 = axs[1, 1].plot(np.average(rho[t_idx, :, :], axis=1), z)
+im4 = axs[1, 1].plot(np.average(rho[t_idx, :, :], axis=1), z, label="rho")
+im4 = axs[1, 1].plot(np.average(rho_d[t_idx, :, :], axis=1), z, label="rho_d")
+axs[1, 1].legend()
 axs[0, 0].set_title("T [K]")
 axs[0, 1].set_title("p [hPa]")
 axs[1, 0].set_title("RH [-]")
